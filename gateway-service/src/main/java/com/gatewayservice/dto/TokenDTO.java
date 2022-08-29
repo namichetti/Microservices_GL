bc524ada01ceabb47abdf77cce7f0eb2773c681e
namichetti <nestor.amichetti@gmail.com>
2022-08-29 17:23:24 -0300

    Add files via upload

---
+++ gateway-service/src/main/java/com/gatewayservice/dto/TokenDTO.java
@@ -0,0 +1,13 @@
+package com.gatewayservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+
+    private String token;
+}
